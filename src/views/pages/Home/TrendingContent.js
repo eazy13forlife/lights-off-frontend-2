f2239a0272d96d8b343c10c742b28ea38745e9bf
Eric B <eric_baffour-addo@alumni.brown.edu>
2022-10-09 23:18:07 -0700

    Upload two pictures, one for a blank person image and another for a blank media image

--- src/views/pages/Home/TrendingContent.js
+++ src/views/pages/Home/TrendingContent.js
@@ -3,6 +3,9 @@ import useTrending from "./useTrending";
 import MediaCard from "../../../components/ContentCards/MediaCard";
 import PersonCard from "../../../components/ContentCards/PersonCard";
 
+import blankPerson from "../../../images/blank-person.jpeg";
+import blankMedia from "../../../images/cinema-clapboard.png";
+
 const TrendingContent = () => {
   //hook for getting current trending results <img src="http://image.tmdb.org/t/p/w500/4tHOplJqH1eg37cFBOWXkBTDTcB.jpg" />
   const trending = useTrending();
@@ -26,7 +29,11 @@ const TrendingContent = () => {
           name={name}
           knownFor={knownFor}
           mediaType={mediaType}
-          image={personImage}
+          image={
+            personImage
+              ? `http://image.tmdb.org/t/p/h632/${personImage}`
+              : blankPerson
+          }
         />
       );
     }
@@ -37,7 +44,11 @@ const TrendingContent = () => {
         title={title}
         releaseDate={releaseDate}
         mediaType={mediaType}
-        image={mediaImage}
+        image={
+          mediaImage
+            ? `http://image.tmdb.org/t/p/w500/${mediaImage}`
+            : blankMedia
+        }
       />
     );
   });
