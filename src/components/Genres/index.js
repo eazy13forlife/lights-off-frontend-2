f38ce3e5defc19f3ba63acd51f7506c46476269f
Eric B <eric_baffour-addo@alumni.brown.edu>
2022-10-21 21:58:35 -0700

    Create Genres component

---
+++ src/components/Genres/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./index.scss";
+const Genres = ({ genres }) => {
+  const renderedGenres = genres.map((genre, index) => {
+    return (
+      <Link to="/" key={index} className="Genres__genre color-light">
+        <span className="heading-medium">{genre}</span>
+      </Link>
+    );
+  });
+
+  return <div className="Genres">{renderedGenres}</div>;
+};
+
+export default Genres;
--- src/views/pages/Movies/index.js
+++ src/views/pages/Movies/index.js
@@ -1,11 +1,36 @@
 import React from "react";
 
 import ContentPageLayout from "../../../components/ContentPageLayout";
+import Genres from "../../../components/Genres";
 
 const Movies = () => {
   return (
     <div className="Movies">
-      <ContentPageLayout></ContentPageLayout>
+      <ContentPageLayout>
+        <Genres
+          genres={[
+            "Action",
+            "Adventure",
+            "Animation",
+            "Comedy",
+            "Crime",
+            "Documentary",
+            "Drama",
+            "Family",
+            "Fantasy",
+            "History",
+            "Horror",
+            "Mystery",
+            "Music",
+            "Romance",
+            "Science Fiction",
+            "TV Movie",
+            "Thriller",
+            "War",
+            "Western",
+          ]}
+        />
+      </ContentPageLayout>
     </div>
   );
 };
