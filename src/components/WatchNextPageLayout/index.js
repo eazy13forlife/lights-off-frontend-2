cf71a7a960603619d3392a434db9e55901700c7f
Eric B <eric_baffour-addo@alumni.brown.edu>
2022-11-25 23:48:37 -0800

    Create WatchNextPageLayout component

---
+++ src/components/WatchNextPageLayout/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import ContentPageLayout from "../ContentPageLayout";
+
+const WatchNextPageLayout = ({ children }) => {
+  const navigate = useNavigate();
+
+  return (
+    <ContentPageLayout
+      searchBarPlaceholder="Search in watch next.."
+      onSearchSubmit={(e, searchValue) => {
+        e.preventDefault();
+        navigate(`/watch-next/search/?name=${searchValue}&page=1`);
+      }}
+    >
+      {children}
+    </ContentPageLayout>
+  );
+};
+
+export default WatchNextPageLayout;
