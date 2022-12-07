6cd1dc4dcfe0ea91b264b8c7f44f06169da5c03a
Eric B <eric_baffour-addo@alumni.brown.edu>
2022-12-06 22:40:23 -0800

    Complete styling for media details pages

--- src/components/MediaDetails/index.js
+++ src/components/MediaDetails/index.js
@@ -13,7 +13,7 @@ import "./index.scss";
 
 const Details = ({ mediaData, castData, mediaType }) => {
   const [displayMessage, setDisplayMessage] = useDisplayMessage("");
-
+  console.log(mediaData);
   const shouldShowDisplayMessage = useDelayUnmount(displayMessage, 0);
 
   //if no media currently, don't show anything on screen.
@@ -24,72 +24,75 @@ const Details = ({ mediaData, castData, mediaType }) => {
   return (
     <div className="Details">
       <div className="Details__container container">
-        <figure className="Details__image-container">
-          <img
-            src={
-              mediaData.poster_path
-                ? `http://image.tmdb.org/t/p/w500/${mediaData.poster_path}`
-                : blankMedia
-            }
-            className="Details__image"
-          />
-          <MediaOptionsButton
-            mediaData={createBackendDataObject(mediaData, mediaType)}
-            setDisplayMessage={setDisplayMessage}
-          />
-        </figure>
+        <div className="Details__top">
+          <figure className="Details__image-container">
+            <img
+              src={
+                mediaData.poster_path
+                  ? `http://image.tmdb.org/t/p/w500/${mediaData.poster_path}`
+                  : blankMedia
+              }
+              className="Details__image"
+            />
+            <MediaOptionsButton
+              mediaData={createBackendDataObject(mediaData, mediaType)}
+              setDisplayMessage={setDisplayMessage}
+            />
+          </figure>
 
-        <div className="Details__info">
-          <div className="Details__group">
-            <div className="Details__header">
-              <h1 className="Details__title">
-                {mediaType === "movie" ? mediaData.title : mediaData.name}
-              </h1>
-              <p className="Details__body-text color-secondary">
-                {mediaType === "movie" ? "movie" : "tv"}
-              </p>
+          <div className="Details__info">
+            <div className="Details__group">
+              <div className="Details__header">
+                <h1 className="Details__title">
+                  {mediaType === "movie" ? mediaData.title : mediaData.name}
+                </h1>
+                <p className="Details__body-text color-secondary">
+                  {mediaType === "movie" ? "movie" : "tv"}
+                </p>
+              </div>
+
+              <p className="Details__tagline">{mediaData.tagline}</p>
             </div>
 
-            <p className="Details__tagline">{mediaData.tagline}</p>
-          </div>
+            {mediaData.imdb_id || mediaData.homepage ? (
+              <div className="Details__group Details__horizontal">
+                {mediaData.imdb_id ? (
+                  <a
+                    href={`https://www.imdb.com/title/${mediaData.imdb_id}/`}
+                    className="Details__external-link"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span className="Details__link-text">IMDB</span>
+                  </a>
+                ) : null}
 
-          {mediaData.imdb_id || mediaData.homepage ? (
+                {mediaData.homepage ? (
+                  <a
+                    href={mediaData.homepage}
+                    className="Details__external-link"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span className="Details__link-text">Website</span>
+                  </a>
+                ) : null}
+              </div>
+            ) : null}
+
             <div className="Details__group Details__horizontal">
-              {mediaData.imdb_id ? (
-                <a
-                  href={`https://www.imdb.com/title/${mediaData.imdb_id}/`}
-                  className="Details__external-link"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <span className="Details__link-text">IMDB</span>
-                </a>
-              ) : null}
-
-              {mediaData.homepage ? (
-                <a
-                  href={mediaData.homepage}
-                  className="Details__external-link"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <span className="Details__link-text">Website</span>
-                </a>
-              ) : null}
+              {RenderedSpecificContent(mediaData, mediaType)}
             </div>
-          ) : null}
 
-          <div className="Details__group Details__horizontal">
-            {RenderedSpecificContent(mediaData, mediaType)}
-          </div>
-
-          <div className="Details__group">
-            <p className="Details__subject">Genres</p>
-            <div className="Details__genres ">
-              {<RenderedGenres genres={mediaData.genres} />}
+            <div className="Details__group">
+              <p className="Details__subject">Genres</p>
+              <div className="Details__genres ">
+                {<RenderedGenres genres={mediaData.genres} />}
+              </div>
             </div>
           </div>
-
+        </div>
+        <div className="Details__bottom">
           <div className="Details__group">
             <p className="Details__subject">Synopsis</p>
             <p className="Details__body-text">{mediaData.overview}</p>
