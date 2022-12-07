0ed01aad95549fab1a37a564ea5ee4208f0ec0a1
Eric B <eric_baffour-addo@alumni.brown.edu>
2022-12-07 03:51:31 -0800

    Complete styling for person details component and pages button component

--- src/views/pages/Person/PersonDetails.js
+++ src/views/pages/Person/PersonDetails.js
@@ -10,67 +10,69 @@ const PersonDetails = ({ data }) => {
   return (
     <div className="Details">
       <div className="Details__container container">
-        <figure className="Details__image-container">
-          <img
-            src={
-              data.profile_path
-                ? `http://image.tmdb.org/t/p/h632/${data.profile_path}`
-                : blankPerson
-            }
-            className="Details__image"
-          />
-        </figure>
+        <div className="Details__top">
+          <figure className="Details__image-container">
+            <img
+              src={
+                data.profile_path
+                  ? `http://image.tmdb.org/t/p/h632/${data.profile_path}`
+                  : blankPerson
+              }
+              className="Details__image"
+            />
+          </figure>
+          <div className="Details__info">
+            <div className="Details__group">
+              <h1 className="Details__title">{data.name}</h1>
+              <p className="Details__tagline">{data.known_for_department}</p>
+            </div>
 
-        <div className="Details__info">
-          <div className="Details__group">
-            <h1 className="Details__title">{data.name}</h1>
-            <p className="Details__tagline">{data.known_for_department}</p>
-          </div>
+            {data.imdb_id || data.homepage ? (
+              <div className="Details__group Details__horizontal">
+                {data.imdb_id ? (
+                  <a
+                    href={`https://www.imdb.com/name/${data.imdb_id}/`}
+                    className="Details__external-link"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span className="Details__link-text">IMDB</span>
+                  </a>
+                ) : null}
 
-          {data.imdb_id || data.homepage ? (
+                {data.homepage ? (
+                  <a
+                    href={data.homepage}
+                    className="Details__external-link"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span className="Details__link-text">Website</span>
+                  </a>
+                ) : null}
+              </div>
+            ) : null}
+
             <div className="Details__group Details__horizontal">
-              {data.imdb_id ? (
-                <a
-                  href={`https://www.imdb.com/name/${data.imdb_id}/`}
-                  className="Details__external-link"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <span className="Details__link-text">IMDB</span>
-                </a>
-              ) : null}
+              <div className="Details__about">
+                <p className="Details__subject">Birthday</p>
+                <p className="Details__body-text">
+                  {data.birthday
+                    ? moment(data.birthday).format("MMM  DD, YYYY")
+                    : "N/A"}
+                </p>
+              </div>
 
-              {data.homepage ? (
-                <a
-                  href={data.homepage}
-                  className="Details__external-link"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <span className="Details__link-text">Website</span>
-                </a>
-              ) : null}
-            </div>
-          ) : null}
-
-          <div className="Details__group Details__horizontal">
-            <div className="Details__about">
-              <p className="Details__subject">Birthday</p>
-              <p className="Details__body-text">
-                {data.birthday
-                  ? moment(data.birthday).format("MMM  DD, YYYY")
-                  : "N/A"}
-              </p>
-            </div>
-
-            <div className="Details__about">
-              <p className="Details__subject">Place of Birth</p>
-              <p className="Details__body-text">
-                {data.place_of_birth ? data.place_of_birth : "N/A"}
-              </p>
+              <div className="Details__about">
+                <p className="Details__subject">Place of Birth</p>
+                <p className="Details__body-text">
+                  {data.place_of_birth ? data.place_of_birth : "N/A"}
+                </p>
+              </div>
             </div>
           </div>
-
+        </div>
+        <div className="Details__bottom">
           <div className="Details__group">
             <p className="Details__subject">Biography</p>
             <p className="Details__body-text">
